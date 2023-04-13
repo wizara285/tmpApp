7a473d5b7d7b879e91128ed2c0277bc37e262b8f
Bartosh_Andrew <bartosha285@gmail.com>
2023-04-13 11:28:58 +0300

    created some object of User

--- tmpApp/Program.cs
+++ tmpApp/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using tmpApp;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        User user = new User()
+        {
+            Id = 1,
+            Name = "Test",
+            Age = 18,
+            Email = "test@gmail.com"
+        };
+        
     }
 }
